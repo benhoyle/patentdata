b0e46429380ea69029a1c1e0fc11b4faa6578723
Ben Hoyle <benjhoyle@gmail.com>
2016-10-26 11:16:27 +0100

    Tidy patentdata namespaces; add utils for common functions

--- patentdata.py
+++ patentdata.py
@@ -9,6 +9,9 @@ import logging
 import epo_ops
 from epo_ops.models import Epodoc
 
+# Import helper utilities
+import utils
+
 chars_to_delete = [".", "&", ",", "/"]
 chars_to_space = ["+","-"]
 
@@ -26,10 +29,16 @@ consumer_key = parser.get('Login Parameters', 'C_KEY')
 consumer_secret = parser.get('Login Parameters', 'C_SECRET')
 
 # Intialise EPO OPS client
-middlewares = [
-    epo_ops.middlewares.Dogpile(),
-    epo_ops.middlewares.Throttler(),
-]
+# Load Dogpile if it exists - if not just use Throttler
+try:
+    middlewares = [
+        epo_ops.middlewares.Dogpile(),
+        epo_ops.middlewares.Throttler(),
+    ]
+except:
+    middlewares = [
+        epo_ops.middlewares.Throttler()
+    ]
 
 registered_client = epo_ops.RegisteredClient(
     key=consumer_key, 
@@ -44,65 +53,10 @@ countries = [line.strip().split("|")[1].upper() for line in open("data/countries
 stopwords = company_stopwords + countries
 
 # Import database objects to store data
-from datamodels import PatentSearch, PatentPublication, Session
+import datamodels
 
-def check_list(listvar):
-    """Turns single items into a list of 1."""
-    if not isinstance(listvar, list):
-        listvar = [listvar]
-    return listvar
-
-def safeget(dct, *keys):
-    """ Recursive function to safely access nested dicts or return None. 
-    param dict dct: dictionary to process
-    param string keys: one or more keys"""
-    for key in keys:
-        try:
-            dct = dct[key]
-        except KeyError:
-            return None
-    return dct
-
-def keysearch(d, key):
-    """Recursive function to look for first occurence of key in multi-level dict. 
-    param dict d: dictionary to process
-    param string key: key to locate"""
- 
-    if isinstance(d, dict):
-        if key in d:
-            return d[key]
-        else:
-            if isinstance(d, dict):
-                for k in d:
-                    found = keysearch(d[k], key)
-                    if found:
-                        return found
-            else:
-                if isinstance(d, list):
-                    for i in d:
-                        found = keysearch(d[k], key)
-                        if found:
-                            return found
-
-# Define helper function to remove text in parenthesis
-# From http://stackoverflow.com/questions/14596884/remove-text-between-and-in-python
-def remove_bracketed(test_str):
-    """ Remove bracketed text from string. """
-    ret = ''
-    skip1c = 0
-    skip2c = 0
-    for i in test_str:
-        if i == '[':
-            skip1c += 1
-        elif i == '(':
-            skip2c += 1
-        elif i == ']' and skip1c > 0:
-            skip1c -= 1
-        elif i == ')'and skip2c > 0:
-            skip2c -= 1
-        elif skip1c == 0 and skip2c == 0:
-            ret += i
-    return ret
+# Import datacache models
+import datacache
 
 def process_name(name):
     """ Clean applicant name for better search."""
@@ -117,7 +71,7 @@ def process_name(name):
     for char in chars_to_space:
         processed_name = processed_name.replace(char, " ")
     # Remove bracketed words
-    processed_name = remove_bracketed(processed_name)   
+    processed_name = utils.remove_bracketed(processed_name)   
     # Delete stopwords 
     import re
     pattern = re.compile(r'\b(' + r'|'.join(stopwords) + r')\b\s*')
@@ -125,12 +79,6 @@ def process_name(name):
     # Get rid of double spaces
     processed_name = processed_name.replace("  ", " ")
     return processed_name.strip()
-
-# Get current year and look for publications in that year
-def get_current_year():
-    import datetime
-    now = datetime.datetime.now()
-    return now.year
 
 def generate_search_string(company_name, year):
     """ Return cql search string given a company_name string and year. """
@@ -140,7 +88,7 @@ def get_search(raw_companies_list):
     """ Get search results for companies in raw_companies_list. """
 
     # Initialise year
-    year = get_current_year()
+    year = utils.get_current_year()
     # Intialise data dump
     data = []
     
@@ -152,11 +100,11 @@ def get_search(raw_companies_list):
             results = registered_client.published_data_search(search_string, range_begin=1, range_end=100)
             results_json = results.json()['ops:world-patent-data']['ops:biblio-search']
            
-            total_results = safeget(results_json, '@total-result-count')
+            total_results = utils.safeget(results_json, '@total-result-count')
             logging.info("Total results: {0}".format(total_results))
-            number_objects = check_list(safeget(results_json,'ops:search-result','ops:publication-reference'))
+            number_objects = utils.check_list(utils.safeget(results_json,'ops:search-result','ops:publication-reference'))
             if number_objects:
-                numbers = [safeget(result,'document-id','country','$') + safeget(result, 'document-id', 'doc-number','$') for result in number_objects]
+                numbers = [utils.safeget(result,'document-id','country','$') + utils.safeget(result, 'document-id', 'doc-number','$') for result in number_objects]
             else:
                 numbers = []
             data.append({
@@ -206,39 +154,36 @@ def get_agent_class(results_json):
     # These can sometimes have multi entries so we'll use checklist to 
     # turn them all into a list (some with only one entry)
     try:
-        raw_agents = check_list(keysearch(results_json,'reg:agents'))
-        raw_classifications = check_list(keysearch(results_json,'reg:classifications-ipcr'))
+        raw_agents = utils.check_list(utils.keysearch(results_json,'reg:agents'))
+        raw_classifications = utils.check_list(utils.keysearch(results_json,'reg:classifications-ipcr'))
         
         if raw_agents:
             agent_details = raw_agents[0]
         else:
-            agent_details = check_list(keysearch(results_json,'reg:applicants'))[0]
+            agent_details = utils.check_list(utils.keysearch(results_json,'reg:applicants'))[0]
         
         # Get first agent / classification
         result_dict = {
-            "agent" : keysearch(agent_details,'reg:name').get('$', None),
-            "agent_first_address" : keysearch(agent_details,'reg:address-1').get('$', None),
-            "agent_country" : keysearch(agent_details,'reg:country').get('$', None),
-            "classification" : keysearch(raw_classifications[0],'reg:text').get('$', None)
+            "agent" : utils.keysearch(agent_details,'reg:name').get('$', None),
+            "agent_first_address" : utils.keysearch(agent_details,'reg:address-1').get('$', None),
+            "agent_country" : utils.keysearch(agent_details,'reg:country').get('$', None),
+            "classification" : utils.keysearch(raw_classifications[0],'reg:text').get('$', None)
             }
     except:
         result_dict = None
     return result_dict
 
-from datacache import RegisterCache
-from datacache import Session as CacheSession
-cachesession = CacheSession()
-
 def save_register(number):
     """Save agent / class data for PatentPublication object number."""
     
-    session = Session()
+    session = datamodels.Session()
+    cachesession = datacache.Session()
     
     publication_number = number.pub_no
     print("Getting register data for {0}".format(publication_number))
 
     # Check for number in cache
-    in_cache = cachesession.query(RegisterCache).filter(RegisterCache.pub_no == publication_number).first()
+    in_cache = cachesession.query(datacache.RegisterCache).filter(datacache.RegisterCache.pub_no == publication_number).first()
 
     if not in_cache:
         # Perform an OPS query
@@ -249,7 +194,7 @@ def save_register(number):
         if json_result:
             # Store result in cache
             try:
-                cache_to_store = RegisterCache(publication_number, json_result)
+                cache_to_store = datacache.RegisterCache(publication_number, json_result)
                 cachesession.add(cache_to_store)
                 cachesession.commit()
             except:
@@ -294,10 +239,10 @@ def getall_registers():
     import math
     import random
     
-    session = Session()
+    session = datamodels.Session()
     
     # Cycle through companies and record agent / classification
-    for entity in session.query(PatentSearch).all():
+    for entity in session.query(datamodels.PatentSearch).all():
         # Sample publication objects if greater than defined number
         if len(entity.publications) > no_of_samples:
             samples = random.sample(entity.publications,no_of_samples)
@@ -311,33 +256,12 @@ def getall_registers():
 
 def get_agent_list(session):
     """ Get list of agent details. """
-    name_list = session.query(PatentPublication.raw_agent, PatentPublication.raw_agent_first_address) \
-        .filter(PatentPublication.raw_agent != None).all()
+    name_list = session.query(datamodels.PatentPublication.raw_agent, datamodels.PatentPublication.raw_agent_first_address) \
+        .filter(datamodels.PatentPublication.raw_agent != None).all()
     name_list_processed = [nl[1] if is_attorney_name(nl[0]) else nl[0] for nl in name_list]
-    fd = list_frequencies(name_list_processed)
-    return sort_freq_dist(fd)
+    fd = utils.list_frequencies(name_list_processed)
+    return utils.sort_freq_dist(fd)
     
-    
-def list_frequencies(list_of_items):
-    """ Determine frequency of items in list_of_items. """
-    itemfreq = [list_of_items.count(p) for p in list_of_items] 
-    return dict(zip(list_of_items,itemfreq))
-
-def sort_freq_dist(freqdict): 
-    """ Sort frequency distribution. """
-    aux = [(freqdict[key], key) for key in freqdict]
-    aux.sort() 
-    aux.reverse() 
-    return aux
-
-def hasNumbers(inputString):
-    return any(char.isdigit() for char in inputString)
-
-import re
-
-def hasReNumbers(inputString):
-    return bool(re.search(r'\d', inputString))
-
 def process_classification(class_string):
     """ Extract IPC classfication elements from a class_string."""
     ipc = r'[A-H][0-9][0-9][A-Z][0-9]{1,4}\/?[0-9]{1,6}' #last bit can occur 1-3 times then we have \d+\\?\d+ - need to finish this
---
+++ utils.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import datetime
+import re
+
+# Common useful utilities
+
+def check_list(listvar):
+    """Turns single items into a list of 1."""
+    if not isinstance(listvar, list):
+        listvar = [listvar]
+    return listvar
+
+def safeget(dct, *keys):
+    """ Recursive function to safely access nested dicts or return None. 
+    param dict dct: dictionary to process
+    param string keys: one or more keys"""
+    for key in keys:
+        try:
+            dct = dct[key]
+        except KeyError:
+            return None
+    return dct
+
+def keysearch(d, key):
+    """Recursive function to look for first occurence of key in multi-level dict. 
+    param dict d: dictionary to process
+    param string key: key to locate"""
+ 
+    if isinstance(d, dict):
+        if key in d:
+            return d[key]
+        else:
+            if isinstance(d, dict):
+                for k in d:
+                    found = keysearch(d[k], key)
+                    if found:
+                        return found
+            else:
+                if isinstance(d, list):
+                    for i in d:
+                        found = keysearch(d[k], key)
+                        if found:
+                            return found
+
+# Define helper function to remove text in parenthesis
+# From http://stackoverflow.com/questions/14596884/remove-text-between-and-in-python
+def remove_bracketed(test_str):
+    """ Remove bracketed text from string. """
+    ret = ''
+    skip1c = 0
+    skip2c = 0
+    for i in test_str:
+        if i == '[':
+            skip1c += 1
+        elif i == '(':
+            skip2c += 1
+        elif i == ']' and skip1c > 0:
+            skip1c -= 1
+        elif i == ')'and skip2c > 0:
+            skip2c -= 1
+        elif skip1c == 0 and skip2c == 0:
+            ret += i
+    return ret
+    
+# Get current year and look for publications in that year
+def get_current_year():
+    """ Get current year. """
+    now = datetime.datetime.now()
+    return now.year
+    
+def list_frequencies(list_of_items):
+    """ Determine frequency of items in list_of_items. """
+    itemfreq = [list_of_items.count(p) for p in list_of_items] 
+    return dict(zip(list_of_items,itemfreq))
+
+def sort_freq_dist(freqdict): 
+    """ Sort frequency distribution. """
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort() 
+    aux.reverse() 
+    return aux
+
+def hasNumbers(inputString):
+    """ Return true if inputString contains numbers. """
+    return any(char.isdigit() for char in inputString)
+
+def hasReNumbers(inputString):
+    """ Return true if inputString contains numbers. Using regex."""
+    return bool(re.search(r'\d', inputString))
