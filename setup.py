086ffb067fdf83954b2efacd5e7303c67565eed3
Ben Hoyle <benjhoyle@gmail.com>
2016-11-02 14:32:16 +0000

    Housekeeping

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='patentdata',
+    version='0.0.1',
+    description='A bag of functions and datamodels for playing around with patent data.',
+    long_description=readme,
+    author='Ben Hoyle',
+    author_email='benjhoyle@gmail.com',
+    url='https://github.com/benhoyle/patentdata',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+    #packages=['patentdata']
+)
